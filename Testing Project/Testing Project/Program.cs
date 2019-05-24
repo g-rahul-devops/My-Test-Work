06df5e54deb96edc06f3c2db123432ffad4d2a25
moorthgo <moorthgo@DEVBOX_GOPI>
2019-05-23 23:46:31 -0700

    checkin testing project

---
+++ Testing Project/Testing Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My testing project");
+            Console.ReadKey();
+
+        }
+    }
+}
